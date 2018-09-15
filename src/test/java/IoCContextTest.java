b2bb7797c7d3951606c6393fe526873a04374d8f
DH-TB <929659475@qq.com>
2018-09-15 16:44:20 +0800

    [ huanglizhen ] step3: add another test file
    Signed-off-by: DH-TB <929659475@qq.com>

--- src/test/java/IoCContextTest.java
+++ src/test/java/IoCContextTest.java
@@ -1,5 +1,4 @@
 import com.example.IoCContextImpl;
-import com.example.interfaceClass.*;
 import com.example.otherClass.MyBean;
 import com.example.otherClass.*;
 import org.junit.jupiter.api.Test;
@@ -94,78 +93,5 @@ class IoCContextTest {
         String actual = bean.getString();
 
         assertEquals("can work", actual);
-    }
-
-    @Test
-    void should_can_work_when_register_interface() throws Exception {
-        IoCContextImpl context = new IoCContextImpl();
-        context.registerBean(MyBeanBase.class, MyBeanImpl.class);
-
-        MyBeanBase bean = context.getBean(MyBeanBase.class);
-
-        assertEquals("bean can work", bean.getString());
-    }
-
-    @Test
-    void should_can_work_when_register_super_class() throws Exception {
-        IoCContextImpl context = new IoCContextImpl();
-        context.registerBean(MyBeanBaseClass.class, MyBeanImpl.class);
-
-        MyBeanBaseClass bean = context.getBean(MyBeanBaseClass.class);
-
-        assertEquals("super class can work", bean.getSuperClass());
-    }
-
-    @Test
-    void should_override_when_register_repeat_same_resolveClazz() throws Exception {
-        IoCContextImpl context = new IoCContextImpl();
-        context.registerBean(MyBeanBase.class, MyBeanImpl.class);
-        context.registerBean(MyBeanBase.class, MyBeanImplAnother.class);
-
-        MyBeanBase bean = context.getBean(MyBeanBase.class);
-
-        assertEquals("override another bean can work", bean.getString());
-    }
-
-    @Test
-    void should_can_get_not_same_bean_when_register_interface() throws Exception {
-        IoCContextImpl context = new IoCContextImpl();
-        context.registerBean(MyBeanBase.class, MyBeanImpl.class);
-
-        MyBeanBase bean = context.getBean(MyBeanBase.class);
-        MyBeanBase beanAnother = context.getBean(MyBeanBase.class);
-
-        assertNotSame(bean, beanAnother);
-    }
-
-    @Test
-    void should_can_get_not_same_bean_when_register_super_class() throws Exception {
-        IoCContextImpl context = new IoCContextImpl();
-        context.registerBean(MyBeanBaseClass.class, MyBeanImpl.class);
-
-        MyBeanBaseClass bean = context.getBean(MyBeanBaseClass.class);
-        MyBeanBaseClass beanAnother = context.getBean(MyBeanBaseClass.class);
-
-        assertNotSame(bean, beanAnother);
-    }
-
-    @Test
-    void should_sub_class_can_get_bean_when_register_interface() throws Exception {
-        IoCContextImpl context = new IoCContextImpl();
-        context.registerBean(MyBeanBase.class, MyBeanImpl.class);
-
-        MyBeanImpl bean = context.getBean(MyBeanImpl.class);
-
-        assertEquals("bean can work", bean.getString());
-    }
-
-    @Test
-    void should_sub_class_can_get_bean_when_register_super_class() throws Exception {
-        IoCContextImpl context = new IoCContextImpl();
-        context.registerBean(MyBeanBaseClass.class, MyBeanImpl.class);
-
-        MyBeanImpl bean = context.getBean(MyBeanImpl.class);
-
-        assertEquals("bean can work", bean.getString());
     }
 }
